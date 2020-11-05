7aa121c9a4a035d2cdcc5c2d0842fddb3e32d0ba
Jon Williamson <jonwilliamson@tamu.edu>
2020-11-04 23:09:57 -0600

    Working final merge

--- create_data.py
+++ create_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 import math
 import operator
 from numpy import mean, nan
+import re
 
 
 def get_point_features():
@@ -28,12 +29,12 @@ def get_team_dict():
 
 def classify_coach_tenure(years):
     if years <= 0:
-        return None
+        return -1
     if years <= 2:
         return 0
     elif years > 2 and years <= 4:
         return 1
-    elif years > 4 and years <= 8:
+    elif years > 4 and years <= 7:
         return 2
     else:
         return 3
@@ -58,6 +59,10 @@ def classify_role(role):
     return "Position"
 
 
+def get_norm_val(rank, out_of):
+    return 1 - (rank - 1) / (out_of - 1)
+
+
 def classify_level(level):
     if "College" in level:
         return "College"
@@ -72,7 +77,7 @@ def quick_mean(lis):
     else:
         return mean(lis)
 
-def get_num_team_in_league(franchise, year, team_path, second_iteration=False):
+def get_num_team_in_league(franchise, year, team_path):
     if not isinstance(franchise, list):
         franchise = [franchise]
     for single in franchise + []:
@@ -99,8 +104,38 @@ def get_hiring_team_stats(franchise, year, team_path):
         "hiring_team_playoff_win":      []
     }
     
+    regex = re.compile(r'[^\d.]+')
     df_record = pd.read_csv(record_path)
-    df_playoff = None if not os.path.exists(playoff_path) else pd.read_csv(playoff_path)
+    for prev_year in [year - 1, year - 2]:
+        record_row = df_record.loc[df_record['Year'] == prev_year]
+        if record_row.shape[0] == 0:
+            continue
+        num_wins = record_row['W'].values[0]
+        num_losses = record_row['L'].values[0]
+        num_ties = record_row['T'].values[0]
+        num_teams = record_row['out of'].values[0]
+        raw_div_text = record_row['Div. Finish'].values[0]
+        raw_div_text = raw_div_text.partition(' of ')
+        div_place = int(regex.sub('', raw_div_text[0]))
+        num_teams_in_division = int(raw_div_text[2])
+        feature_dict["hiring_team_win_pct"].append((num_wins + .5 * num_ties) / (num_losses + num_ties + num_wins))
+        feature_dict["hiring_team_norm_turnover"].append(get_norm_val(record_row['T/G'].values[0], num_teams))
+        feature_dict["hiring_team_norm_point"].append(get_norm_val(record_row['Pts±'].values[0], num_teams))
+        feature_dict["hiring_team_norm_yard"].append(get_norm_val(record_row['Yds±'].values[0], num_teams))
+        feature_dict["hiring_team_norm_div"].append(get_norm_val(div_place, num_teams_in_division))
+    
+    
+    if os.path.exists(playoff_path):
+        df_playoff = pd.read_csv(playoff_path)
+        for prev_year in [year - 1, year - 2]:
+            record_rows = df_playoff.loc[df_playoff['Year'] == prev_year]
+            num_games = record_rows.shape[0]
+            if num_games == 0:
+                feature_dict["hiring_team_playoff_app"].append(0)
+                feature_dict["hiring_team_playoff_win"].append(0)
+            else:
+                feature_dict["hiring_team_playoff_app"].append(1)
+                feature_dict["hiring_team_playoff_win"].append(sum(1 for game_result in record_rows.iloc[:,6].values if game_result == 'W'))
 
     return [quick_mean(value) for value in feature_dict.values()]
 
@@ -141,13 +176,13 @@ def parse_coach_career(coach_name, coach_path, team_path):
         "nfl_hc_norm_point":    quick_mean,
         "nfl_hc_norm_turnover": quick_mean
     }
-    
     dfs = []
     for table_name in table_names:
         dfs.append(pd.read_csv(file_path + table_name + ".csv"))
     # For each year in their career
     prev_franchise_abrev = None
     is_head_coach = False
+    year = 0
     for row in dfs[2].itertuples(index=False):
         level = classify_level(row[3])
         role = classify_role(row[5])
@@ -156,6 +191,8 @@ def parse_coach_career(coach_name, coach_path, team_path):
         if is_head_coach and (level == 'College' or role != "Head Coach"):
             is_head_coach = False
             feature_dict["demotion_presence"] = 1
+            prev_year = rows[-1][1]
+            rows[-1].append(classify_coach_tenure(year - prev_year))
 
         if level == "College":
             if role == "Position":
@@ -182,36 +219,73 @@ def parse_coach_career(coach_name, coach_path, team_path):
                 if role == "Offensive Coordinator" or role == "Defensive Coordinator" or role == "Special Teams Coordinator":
                     feature_dict["num_yr_nfl_coor"] += 1
                     if role == "Offensive Coordinator":
-                        feature_dict["nfl_oc_norm_yard"].append(dfs[1].loc[dfs[1]['Year'] == year]['Yds'].values[0] / num_teams_in_league)
-                        feature_dict["nfl_oc_norm_point"].append(dfs[1].loc[dfs[1]['Year'] == year]['Pts'].values[0] / num_teams_in_league)
-                        feature_dict["nfl_oc_norm_giveaway"].append(dfs[1].loc[dfs[1]['Year'] == year]['GvA'].values[0] / num_teams_in_league)
+                        feature_dict["nfl_oc_norm_yard"].append(get_norm_val(dfs[1].loc[dfs[1]['Year'] == year]['Yds'].values[0], num_teams_in_league))
+                        feature_dict["nfl_oc_norm_point"].append(get_norm_val(dfs[1].loc[dfs[1]['Year'] == year]['Pts'].values[0], num_teams_in_league))
+                        feature_dict["nfl_oc_norm_giveaway"].append(get_norm_val(dfs[1].loc[dfs[1]['Year'] == year]['GvA'].values[0], num_teams_in_league))
                     elif role == "Defensive Coordinator":
-                        feature_dict["nfl_dc_norm_yard"].append(dfs[1].loc[dfs[1]['Year'] == year]['Yds Passing Off'].values[0] / num_teams_in_league)
-                        feature_dict["nfl_dc_norm_point"].append(dfs[1].loc[dfs[1]['Year'] == year]['Pts Passing Off'].values[0] / num_teams_in_league)
-                        feature_dict["nfl_dc_norm_turnover"].append(dfs[1].loc[dfs[1]['Year'] == year]['TkA'].values[0] / num_teams_in_league)
+                        feature_dict["nfl_dc_norm_yard"].append(get_norm_val(dfs[1].loc[dfs[1]['Year'] == year]['Yds Passing Off'].values[0], num_teams_in_league))
+                        feature_dict["nfl_dc_norm_point"].append(get_norm_val(dfs[1].loc[dfs[1]['Year'] == year]['Pts Passing Off'].values[0], num_teams_in_league))
+                        feature_dict["nfl_dc_norm_turnover"].append(get_norm_val(dfs[1].loc[dfs[1]['Year'] == year]['TkA'].values[0], num_teams_in_league))
                     # Nothing necessary if special teams
                 elif role == "Head Coach":
                     if not is_head_coach or franchise_abrev != prev_franchise_abrev:
                         is_head_coach = True
                         feature_dict["age"] = row[2]
-                        # TODO fix check
-                        prev_team = dfs[1].loc[dfs[1]['Year'] == year]['Tm'].values[0].lower()
-                        prev_franchise_abrev = team_dict.get(prev_team, prev_team)
+                        prev_franchise_abrev = franchise_abrev
+                        winning_result = []
+                        for new_year in [year, year + 1]:
+                            record_row = dfs[0].loc[dfs[0]['Year'] == new_year]
+                            if record_row.shape[0] == 0:
+                                continue
+                            num_wins = record_row['W'].values[0]
+                            num_losses = record_row['L'].values[0]
+                            num_ties = record_row['T'].values[0]
+                            winning_result.append((num_wins + .5 * num_ties) / (num_wins + num_ties + num_losses))
+                        # Not first time hire, previous career to calculate
+                        if len(rows) != 0 and len(rows[-1]) == 28:
+                            prev_year = rows[-1][1]
+                            rows[-1].append(classify_coach_tenure(year - prev_year))
 
                         # Adding the data
                         new_row = [coach_name, year] + [value if key not in feature_transform_dict else feature_transform_dict[key](value) for key, value in feature_dict.items()]
-                        #new_row += get_hiring_team_stats(franchise_abrev, year, team_path)
+                        new_row += get_hiring_team_stats(franchise_abrev, year, team_path)
+                        new_row.append(quick_mean(winning_result))
                         
                         # add a row
                         rows.append(new_row)
-                    feature_dict["nfl_hc_norm_yard"].append(dfs[1].loc[dfs[1]['Year'] == year]['Yds±'].values[0] / num_teams_in_league)
-                    feature_dict["nfl_hc_norm_point"].append(dfs[1].loc[dfs[1]['Year'] == year]['Pts±'].values[0] / num_teams_in_league)
-                    feature_dict["nfl_hc_norm_turnover"].append(dfs[1].loc[dfs[1]['Year'] == year]['T/G'].values[0] / num_teams_in_league)
+                    feature_dict["nfl_hc_norm_yard"].append(get_norm_val(dfs[1].loc[dfs[1]['Year'] == year]['Yds±'].values[0], num_teams_in_league))
+                    feature_dict["nfl_hc_norm_point"].append(get_norm_val(dfs[1].loc[dfs[1]['Year'] == year]['Pts±'].values[0], num_teams_in_league))
+                    feature_dict["nfl_hc_norm_turnover"].append(get_norm_val(dfs[1].loc[dfs[1]['Year'] == year]['T/G'].values[0], num_teams_in_league))
                     feature_dict["num_times_hc"] += 1
                     feature_dict["num_yr_nfl_hc"] += 1
                     # Add
-        
-    #print(feature_dict)
+    
+    #Handle final hire length if not demotion
+    if len(rows) != 0 and len(rows[-1]) == 28:
+        prev_year = rows[-1][1]
+        # Does not consider hires made in 2020 as they do not have 
+        # at least one full season of coached games
+        cutoff_year = 2014
+        current_year = 2020
+        if prev_year == current_year:
+            rows.pop()
+            print('\tExcluded last hire for both classification: {}, hire year: {}'.format(coach_name, prev_year))
+        else:
+            # Necessary as the year count is not iterated like all previous checks
+            year += 1
+            # Does not handle current coach tenure classification for those
+            # hired since 2014 and still employed since not enough time has passed 
+            # for fair classification
+            if year > current_year and prev_year >= cutoff_year:
+                print('\tExcluded last hire tenure classification: {}'.format(coach_name))
+            rows[-1].append(classify_coach_tenure(year - prev_year) if prev_year < cutoff_year else -1)
+
+
+    #Checks length
+    for i in range(0, len(rows)):
+        if len(rows[i]) != 29:
+            print('Error: {} {}'.format(coach_name, rows[i]))
+    #print(rows)
     return rows
 
 
@@ -229,11 +303,11 @@ def main():
         for new_row in parse_coach_career(coach_name, coach_path, team_path):
             master_data.append(new_row)
         count += 1
-        """if count == 2:
+        """if count == 20:
             break"""
-    #df = pd.DataFrame(data=master_data, columns=get_point_features())
-    #df.to_csv("master_data.csv")
-    #df.to_feather("master_data.feather")
+    df = pd.DataFrame(data=master_data, columns=get_point_features())
+    df.to_csv("master_data.csv")
+    df.to_feather("master_data.feather")
     print(len(master_data))
 
 if __name__ == "__main__":
